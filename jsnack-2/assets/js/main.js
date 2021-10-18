6fdda8ae88fea7d5085f220acd8ac7ff1057a335
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-18 15:51:26 +0200

    Completo jsnack-2

---
+++ jsnack-2/assets/js/main.js
@@ -0,0 +1,33 @@
+const containerElement = document.querySelector(".container");
+
+/*JSnack 2*/
+const userWord_1 = prompt("Inserisci la prima parola");
+const userWord_2 = prompt("Inserisci la seconda parola");
+
+if (userWord_1.length < userWord_2.length) {
+    //console.log(userWord_1, userWord_2);
+    containerElement.insertAdjacentHTML("beforeend", userWord_1)
+    containerElement.insertAdjacentHTML("beforeend", " ")
+    containerElement.insertAdjacentHTML("beforeend", userWord_2)
+
+}
+else if (userWord_1.length > userWord_2.length) {
+    containerElement.insertAdjacentHTML("beforeend", userWord_2)
+    containerElement.insertAdjacentHTML("beforeend", " ")
+    containerElement.insertAdjacentHTML("beforeend", userWord_1)
+    //console.log(userWord_2, userWord_1);
+}
+else {
+    containerElement.insertAdjacentHTML("beforeend", userWord_1)
+    containerElement.insertAdjacentHTML("beforeend", " ")
+    containerElement.insertAdjacentHTML("beforeend", userWord_2)
+    containerElement.insertAdjacentHTML("beforeend", " Le parole hanno la stessa lunghezza");
+    //console.log(userWord_1, userWord_2);
+    //console.log("Le parole hanno la stessa lunghezza");
+}
+
+
+
+
+
+
