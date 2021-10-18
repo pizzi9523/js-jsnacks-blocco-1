b11426111e671c1d012250c929196e67d85d36f3
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-18 17:27:58 +0200

    vado a capo per separare le parole jsnack-2

--- jsnack-2/assets/js/main.js
+++ jsnack-2/assets/js/main.js
@@ -7,21 +7,21 @@ const userWord_2 = prompt("Inserisci la seconda parola");
 if (userWord_1.length < userWord_2.length) {
     //console.log(userWord_1, userWord_2);
     containerElement.insertAdjacentHTML("beforeend", userWord_1)
-    containerElement.insertAdjacentHTML("beforeend", " ")
+    containerElement.insertAdjacentHTML("beforeend", "<br>")
     containerElement.insertAdjacentHTML("beforeend", userWord_2)
 
 }
 else if (userWord_1.length > userWord_2.length) {
     containerElement.insertAdjacentHTML("beforeend", userWord_2)
-    containerElement.insertAdjacentHTML("beforeend", " ")
+    containerElement.insertAdjacentHTML("beforeend", "<br>")
     containerElement.insertAdjacentHTML("beforeend", userWord_1)
     //console.log(userWord_2, userWord_1);
 }
 else {
     containerElement.insertAdjacentHTML("beforeend", userWord_1)
-    containerElement.insertAdjacentHTML("beforeend", " ")
+    containerElement.insertAdjacentHTML("beforeend", "<br>")
     containerElement.insertAdjacentHTML("beforeend", userWord_2)
-    containerElement.insertAdjacentHTML("beforeend", " Le parole hanno la stessa lunghezza");
+    containerElement.insertAdjacentHTML("beforeend", "<br> Le parole hanno la stessa lunghezza");
     //console.log(userWord_1, userWord_2);
     //console.log("Le parole hanno la stessa lunghezza");
 }
